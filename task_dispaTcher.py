81a3b4b5f585c809ccc8c3eb099937c1445ea91d
Hossein Aghaei <59363901+hoseinaghaei@users.noreply.github.com>
2024-01-06 13:06:31 +0330

    remove print

--- task_dispaTcher.py
+++ task_dispaTcher.py
@@ -4,5 +4,4 @@ import binpacking
 
 def dispatch_tasks(tasks: list[Task], processor_count: int) -> list:
     task_index_to_utilization = dict(map(lambda x: (x[0], x[1].utilization), enumerate(tasks)))
-    print(task_index_to_utilization)
     return binpacking.to_constant_bin_number(task_index_to_utilization, processor_count)
