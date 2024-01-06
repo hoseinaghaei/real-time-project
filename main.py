d6a86e44e6177ec8be14838c2ac1253c606a9393
armin <armin.delgosarmaher@huma.com>
2024-01-06 18:22:40 +0330

    Complete main

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import task
-import task_dispaTcher
+import task_dispatcher
 import uunifast
 from processor import Processor
+from scheduler import Scheduler
 
 
 def run(utilization: float):
-    task_count = 100
+    task_count = 50
     processor_count = 8
+    processor_max_f = 1.6
+    processor_min_f = 1.2
+    scheduling_upperbound = 1200
 
     uunifast_utilization_list = uunifast.generate_uunifast_discard(utilization * processor_count, task_count)
     tasks = task.generate_tasks_from_utilization(uunifast_utilization_list, "task.csv")
 
-    dispatched_tasks = task_dispaTcher.dispatch_tasks(tasks, processor_count)
+    dispatched_tasks = task_dispatcher.dispatch_tasks(tasks, processor_count)
 
-    processors = list(map(lambda x: Processor(x[0] + 1, [tasks[i] for i in x[1]]), enumerate(dispatched_tasks)))
-    print(processors)
-    # todo : @ARMIN
+    processors = list(
+        map(lambda x: Processor(x[0] + 1, [tasks[i] for i in x[1]], min_f=processor_min_f, max_f=processor_max_f),
+            enumerate(dispatched_tasks)))
+    scheduler = Scheduler(processors=processors, scheduling_upperbound=scheduling_upperbound)
+    scheduler.schedule()
+    logs = scheduler.get_logs()
+    # todo : @HOSSEIN
 
 
 if __name__ == '__main__':
